617f25673c77c9b49a949f1b175e88307195c6f1
James Wyn <thisisjameswyn@gmail.com>
2021-02-02 12:15:15 -0800

    add files that are from 2/1/2021
    Co-Authored by: Olha Hizhytska <olgainfotech@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -7,3 +7,42 @@ Journal.prototype.evalString = function() {
   let myString = this.body.split(" ").length;
   console.log(myString);
 }
+
+Journal.prototype.gimmeAte = function() {
+  let myString = this.body.split(" ");
+  let upToAte = [];
+  for(let i = 0; i < 8; i++)  {
+    if(myString[i] != undefined && myString[i] != " ") {
+      upToAte.push(myString[i]);
+    }
+  }
+  console.log(upToAte);
+}
+
+Journal.prototype.howMany = function() {
+  let vCounter = 0;
+  let cCounter = 0;
+  let oCounter = 0;
+  let vowel = this.body.split("");
+  console.log(vowel);
+  const compVowel = ["a", "e", "i", "o", "u", "y"];
+  const compOthers = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+  vowel.forEach(function (element) {
+    if (compVowel.includes(element)) {
+      vCounter ++;
+    } else if (compOthers.includes(element)) {
+      oCounter ++;
+    } else {
+      cCounter ++;
+    }
+  });
+    console.log(vCounter, oCounter, cCounter);
+};
+
+// var myStringArray = ["Hello","World"];
+// var arrayLength = myStringArray.length;
+// for (var i = 0; i < arrayLength; i++) {
+//     console.log(myStringArray[i]);
+    //Do something
+// 
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import $ from 'jquery';
 import './css/styles.css';
 import Journal from './journal.js';
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
+  $('#journal').submit(function(event) {
     event.preventDefault();
-    // const length1 = $('#length1').val();
+    const textEntry = $('#textOne').val();
+    console.log(textEntry);
     // const length2 = $('#length2').val();
     // const length3 = $('#length3').val();
-    const journal = new Journal("my JOurnal", "Here is the text for it, also, olha loves coding")
-    const response = journal.evalString();
-    $('#response').append("<p>" + response + "</p>");
+    const journal = new Journal("my JOurnal", textEntry)
+    journal.gimmeAte();
+    // $('#response').append("<p>" + response + "</p>");
   });
 });
